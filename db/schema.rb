f870092a65cdcb96640c9ab0270d7c53086d58cc
Katie Lonsdale <katie952@gmail.com>
2023-07-24 16:19:17 -0600

    db: change default value for subscription status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_24_210839) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_221658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_24_210839) do
     t.bigint "customer_id", null: false
     t.string "title"
     t.string "price"
-    t.integer "status", default: 1
+    t.integer "status", default: 0
     t.string "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
