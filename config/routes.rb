400efe2c098c8073d8ce77375c69026e57f6804f
Katie Lonsdale <katie952@gmail.com>
2023-07-25 16:39:23 -0600

    add route for destroy subscription

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :customers, only: [:show] do
-        resources :subscriptions, only: [:create]
+        resources :subscriptions, only: [:create, :destroy]
       end
     end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
