10c5acf1d596ab4fa859e9656098e50856ee7cec
Katie Lonsdale <katie952@gmail.com>
2023-07-25 16:38:40 -0600

    feat: add destroy action

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,9 +1,16 @@
 class Api::V0::SubscriptionsController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :find_customer
 
   def create
     sub = Subscription.create!(subscription_params)
     render json: SubscriptionSerializer.new(sub), status: 201
+  end
+
+  def destroy
+    subscription = Subscription.find(params[:id])
+    subscription.destroy!
   end
 
   private
@@ -13,5 +20,13 @@ class Api::V0::SubscriptionsController < ApplicationController
 
   def record_invalid(error)
     render json: ErrorSerializer.error_message(error), status: 400
+  end
+
+  def record_not_found(error)
+    render json: ErrorSerializer.error_message(error), status: 404
+  end
+
+  def find_customer
+    Customer.find(params[:customer_id])
   end
 end
