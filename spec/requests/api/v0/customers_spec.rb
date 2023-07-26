6a7da60b42a23c28f05cf391fa60014aecf5ec3b
Katie Lonsdale <katie952@gmail.com>
2023-07-26 14:17:04 -0600

    test: add get all customer subscriptions sad path test

--- spec/requests/api/v0/customers_spec.rb
+++ spec/requests/api/v0/customers_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Customers", type: :request do
   describe "get all customer subscriptions" do
+    before(:each) do
+      @customer = create(:customer)
+      active_subs = create(:subscription, status: 0, customer_id: @customer.id)
+      cancelled_subs = create_list(:subscription, 4, status: 1, customer_id: @customer.id)
+    end
     it "returns all customer subscriptions" do
-      customer = create(:customer)
-      active_subs = create(:subscription, status: 0, customer_id: customer.id)
-      cancelled_subs = create_list(:subscription, 4, status: 1, customer_id: customer.id)
-
-      get "/api/v0/customers/#{customer.id}/subscriptions"
+      get "/api/v0/customers/#{@customer.id}/subscriptions"
 
       expect(response.status).to eq(200)
       data = JSON.parse(response.body, symbolize_names: true)
@@ -25,6 +26,13 @@ RSpec.describe "Customers", type: :request do
       attributes.each do |attribute|
         expect(sub_attributes).to have_key(attribute)
       end
+    end
+    it "returns a 404 error if user does not exist" do
+      get "/api/v0/customers/04/subscriptions"
+
+      expect(response.status).to eq(404)
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data.dig(:errors, 0, :detail)).to eq("Couldn't find Customer with 'id'=04")
     end
   end
 end
