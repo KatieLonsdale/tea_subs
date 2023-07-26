a6f98226e298fa7f6de105e82508ad10b5383c92
Katie Lonsdale <katie952@gmail.com>
2023-07-26 14:08:14 -0600

    test: add test for get all customer subscriptions

--- spec/requests/api/v0/customers_spec.rb
+++ spec/requests/api/v0/customers_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Customers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "get all customer subscriptions" do
+    it "returns all customer subscriptions" do
+      customer = create(:customer)
+      active_subs = create(:subscription, status: 0, customer_id: customer.id)
+      cancelled_subs = create_list(:subscription, 4, status: 1, customer_id: customer.id)
+
+      get "/api/v0/customers/#{customer.id}/subscriptions"
+
+      expect(response.status).to eq(200)
+      data = JSON.parse(response.body, symbolize_names: true)
+      subs = data[:data]
+
+      expect(subs.count).to eq(5)
+      subs.each do |sub|
+        expect(sub).to have_key(:id)
+        expect(sub[:type]).to eq("subscription")
+        expect(sub).to have_key(:attributes)
+      end
+
+      sub_attributes = subs.dig(0, :attributes)
+      attributes = [:customer_id, :title, :price, :status, :frequency]
+      attributes.each do |attribute|
+        expect(sub_attributes).to have_key(attribute)
+      end
+    end
   end
 end
